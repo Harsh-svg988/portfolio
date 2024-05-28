379bf7fe288650b686be38b2ce7f48ce9aec133e
Harsh-svg988 <harshprabhakar305.com>
2024-05-28 23:52:53 +0530

    added html fot footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
 import HomePage from './HomePage.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HomePage/>
+      <Footer/>
     </div>
   );
 }
